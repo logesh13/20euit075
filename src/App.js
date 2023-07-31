6cf418a4bcfe1490a087bbd5629a1ff01e1ea4f2
LOGESH S <105351999+logesh13@users.noreply.github.com>
2023-07-31 18:34:24 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
+import Main from './Main.css'
 function App() {
   return (
     <div className="App">
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-
+// import Main from '/src/Main.css';
 const Home = () => {
   const [uidata, setuidata] = useState([]);
 
@@ -14,12 +14,38 @@ const Home = () => {
   };
   return (
     <div>
-      <h1>Train Information</h1>
+      <h1>Logi - Travel - Booking</h1>
       <ul>
         {uidata.map(train => (
           <li key={train.id}>
-            {train.trainName} - Departure Time: {formatTime(train.departureTime)}
-            {train.seatsAvailable }
+            <div className='batch'>
+                <div className='batchinside'>
+                <b>Train :- </b>{train.trainName} 
+                <br/>
+                <b>Train Number :- </b>{train.trainNumber}
+                <br/>
+                <b>Delayed By :- </b>{train.delayedBy}
+                
+
+                
+
+                
+
+                </div>
+                <div className='dept'>
+                    <b>Train Departs At :  </b>{formatTime(train.departureTime)}
+                </div>
+                <div className='seats'>
+                    <b>Seats Available : </b><br/>Sleeper : {train.seatsAvailable.sleeper}<br/>
+                    AC : {train.seatsAvailable.AC}
+                </div>
+                <div className='seats2'>
+                    Price  : <b>{train.price.sleeper}</b>
+                </div>
+                
+            </div>
+            
+            
           </li>
         ))}
       </ul>
@@ -28,5 +54,4 @@ const Home = () => {
 };
 
 export default Home;
-
 
