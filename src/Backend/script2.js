c1a6d5d974393efd27dcdf8a71762e6167445d68
LOGESH S <105351999+logesh13@users.noreply.github.com>
2023-07-31 17:43:37 +0530

    Data Fetched in React

--- src/Backend/script2.js
+++ src/Backend/script2.js
@@ -1,25 +1,55 @@
+// const express = require('express');
+// const app = express();
+// app.use(express.json())
+// const axios = require('axios');
+// const urll = "http://20.244.56.144/train/trains";
+// const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTA4MDQ3NTQsImNvbXBhbnlOYW1lIjoiTG9nZXNoLVRyYXZlbC1UaW1lIiwiY2xpZW50SUQiOiIxNTBlZmIxZi1hM2I2LTRmYjktODFiZS1iYjA0ODExMTBhYTYiLCJvd25lck5hbWUiOiIiLCJvd25lckVtYWlsIjoiIiwicm9sbE5vIjoiMjBFVUlUMDc1In0.ocNLZ6cnbzxvGK5isoADFq_cfoKfyIIZIe4yYGYVryA";
+// const restemp1 =[];
+// const ress =[{}];
+// app.get('/api/data', async (req, res) => {
+//     try {
+//       const url2 = urll; 
+//       const headers = { Authorization: `Bearer ${token}` };
+//       const resp = await axios.get(url2, { headers });
+//       restemp = resp.data;
+//       res.json(restemp);
+//       console.log(JSON.stringify(restemp));
+//     } catch (error) {
+//       console.error('Error fetching data:', error.message);
+//       res.status(500).json({ error: 'Error fetching data from the server.' });
+//     }
+//   });
+//   app.listen(7000 , () =>{
+//     console.log('server is Running')
+// })
 const express = require('express');
+const cors = require('cors'); // Import the cors middleware
 const app = express();
-app.use(express.json())
+app.use(express.json());
+
 const axios = require('axios');
 const urll = "http://20.244.56.144/train/trains";
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTA4MDQyMzAsImNvbXBhbnlOYW1lIjoiTG9nZXNoLVRyYXZlbC1UaW1lIiwiY2xpZW50SUQiOiIxNTBlZmIxZi1hM2I2LTRmYjktODFiZS1iYjA0ODExMTBhYTYiLCJvd25lck5hbWUiOiIiLCJvd25lckVtYWlsIjoiIiwicm9sbE5vIjoiMjBFVUlUMDc1In0.VdsGZKhk2UghZlz7MH6vSiLbGKduHcZyfp-3rBevD3I";
-const restemp1 =[];
-const ress =[{}];
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTA4MDU3NjIsImNvbXBhbnlOYW1lIjoiTG9nZXNoLVRyYXZlbC1UaW1lIiwiY2xpZW50SUQiOiIxNTBlZmIxZi1hM2I2LTRmYjktODFiZS1iYjA0ODExMTBhYTYiLCJvd25lck5hbWUiOiIiLCJvd25lckVtYWlsIjoiIiwicm9sbE5vIjoiMjBFVUlUMDc1In0.-tknBcGvEykHeDqoDwWclokU4XNdk6lGXW3t3QVVCvQ";
+const restemp1 = [];
+const ress = [{}];
+
+// Use the cors middleware to allow requests from all origins
+app.use(cors());
+
 app.get('/api/data', async (req, res) => {
-    try {
-      const url2 = urll; 
-      const headers = { Authorization: `Bearer ${token}` };
-      const resp = await axios.get(url2, { headers });
-      restemp = resp.data;
-      res.json(restemp);
-      console.log(JSON.stringify(restemp));
-    } catch (error) {
-      console.error('Error fetching data:', error.message);
-      res.status(500).json({ error: 'Error fetching data from the server.' });
-    }
-  });
-  app.listen(7000 , () =>{
-    console.log('server is Running')
-})
- 
+  try {
+    const url2 = urll;
+    const headers = { Authorization: `Bearer ${token}` };
+    const resp = await axios.get(url2, { headers });
+    restemp = resp.data;
+    res.json(restemp);
+    console.log(JSON.stringify(restemp));
+  } catch (error) {
+    console.error('Error fetching data:', error.message);
+    res.status(500).json({ error: 'Error fetching data from the server.' });
+  }
+});
+
+app.listen(7000, () => {
+  console.log('server is Running');
+});
---
+++ src/components/Home.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+
+const Home = () => {
+  const [uidata, setuidata] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:7000/api/data')
+      .then(response => response.json())
+      .then(data => setuidata(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  const formatTime = (timeObject) => {
+    return `${timeObject.Hours}:${timeObject.Minutes}:${timeObject.Seconds}`;
+  };
+
+  return (
+    <div>
+      <h1>Train Information</h1>
+      <ul>
+        {uidata.map(train => (
+          <li key={train.id}>
+            {train.trainName} - Departure Time: {formatTime(train.departureTime)}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
+
+
